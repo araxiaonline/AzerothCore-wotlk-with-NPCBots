araxiaonline/AzerothCore-wotlk-with-NPCBots

commit 4f4e4263509fae890a241e3ea9dd7fc9d10d831e

Fix configs in docker builds (#14747)
<!-- First of all, THANK YOU for your contribution. -->

## Changes Proposed
- This fixes an issue where the .conf files aren't properly generated for the docker build
  - With this issue, the dbimport service in the docker compose file would fail to start due to insufficient config. 
  - related to PR #14707
- create a ".user" file extension for custom configs in `env/docker/etc/`
- Layers the ".dockerdist" file and the ".user" file over the ".dist" file (in that order), and then copies the ".dist" file to the base ".conf" file
- Docker specific config changes go in the ".dockerdist" file and should still be committed to the repo
- User specific config changes go in ".user" file and should not be committed to the repo
- If the ".dist" file and the base ".conf" file are different, the ".conf" will be backed up before the ".dist" file is copied into it

## Issues Addressed:
<!-- If your fix has a relating issue, link it below -->
- There isn't a specific issue, however this was reported and discussed in discord over the course of yesterday and today

## SOURCE:
<!-- If you can, include a source that can strengthen your claim -->

## Tests Performed:
<!-- Does it build without errors? Did you test in-game? What did you test? On which OS did you test? Describe any other tests performed -->
- Executing commands to build and start the game server
- after creating an account, I can log in and play without any issues. 


## How to Test the Changes:
<!-- Describe in a detailed step-by-step order how to test the changes -->

1.  Execute commands
   
    ```bash
    $ ./acore.sh docker clean:build && ./acore.sh docker build:nocache && ./acore.sh docker start:app
    ```
2. Create account and log in as normal

## Known Issues and TODO List:
<!-- Is there anything else left to do after this PR? -->

apps/compiler/includes/functions.sh
@@ -104,9 +104,7 @@ function comp_compile() {
 
   echo "Using $MTHREADS threads"
 
-  CWD=$(pwd)
-
-  cd $BUILDPATH
+  pushd "$BUILDPATH" >> /dev/null || exit 1
 
   comp_ccacheEnable
 
@@ -121,7 +119,7 @@ function comp_compile() {
     msys*)
       cmake --install . --config $CTYPE
 
-      cd $CWD
+      popd >> /dev/null || exit 1
 
       echo "Done"
       ;;
@@ -138,7 +136,7 @@ function comp_compile() {
       echo "Cmake install..."
       sudo cmake --install . --config $CTYPE
 
-      cd $CWD
+      popd >> /dev/null || exit 1
 
       # set all aplications SUID bit
       echo "Setting permissions on binary files"
@@ -149,13 +147,16 @@ function comp_compile() {
 
       if [[ $DOCKER = 1 && $DISABLE_DOCKER_CONF != 1 ]]; then
         echo "Generating confs..."
-        cp -n "$DOCKER_ETC_FOLDER/worldserver.conf.dockerdist" "${confDir}/worldserver.conf"
-        cp -n "$DOCKER_ETC_FOLDER/authserver.conf.dockerdist" "${confDir}/authserver.conf"
-        cp -n "$DOCKER_ETC_FOLDER/dbimport.conf.dockerdist" "${confDir}/dbimport.conf"
+        for dockerdist in "$DOCKER_ETC_FOLDER"/*.dockerdist; do
+          base_conf="$(echo "$dockerdist" | rev | cut -f1 -d. --complement | rev)"
+          cp -nv "$base_conf.dist" "$base_conf"
+          # Move configs from .conf.dockerdist to .conf
+          conf_layer "$dockerdist" "$base_conf" " # Copied from dockerdist"
+        done
       fi
 
       echo "Done"
-    ;;
+      ;;
   esac
 
   runHooks "ON_AFTER_BUILD"
@@ -169,4 +170,32 @@ function comp_build() {
 function comp_all() {
   comp_clean
   comp_build
+}
+
+# conf_layer FILENAME FILENAME
+# Layer the configuration parameters from the first argument onto the second argument
+function conf_layer() {
+  LAYER="$1"
+  BASE="$2"
+  COMMENT="$3"
+
+  grep -E "^[a-zA-Z\.0-9]+\s*=.*$" "$LAYER" \
+    | while read -r param
+      do
+        NOSPACE="$(tr -d '[:space:]' <<< "$param")"
+        KEY="$(cut -f1 -d= <<< "$NOSPACE")"
+        VAL="$(cut -f2 -d= <<< "$NOSPACE")"
+        # if key not in base or val not in line
+        if grep -qE "^$KEY" "$BASE" && ! grep -qE "^$KEY.*=.*$VAL" "$BASE"; then
+          # Replace line
+          # Prevent issues with shell quoting 
+          sed -i \
+            's,^'"$KEY"'.*,'"$KEY = $VAL$COMMENT"',g' \
+            "$BASE"
+        else
+          # insert line
+          echo "$KEY = $VAL$COMMENT" >> "$BASE"
+        fi
+      done
+  echo "Layered $LAYER onto $BASE"
 }

.gitignore
@@ -22,9 +22,7 @@
 !/env/docker/data/.gitkeep
 !/env/docker/etc/
 /env/docker/etc/*
-!/env/docker/etc/authserver.conf.dockerdist
-!/env/docker/etc/worldserver.conf.dockerdist
-!/env/docker/etc/dbimport.conf.dockerdist
+!/env/docker/etc/*.conf.dockerdist
 !/env/docker/logs/.gitkeep
 /.env*
 /apps/joiner

commit b77dc309fc4a6fe8292b900a75b7434b9b6f1edc

fix(Docker): config loading for ac-dev-server container (#14755)
## Changes Proposed:
-  This properly moves things from `DOCKER_ETC_FOLDER` to `confDir`
- originally broken with
https://github.com/azerothcore/azerothcore-wotlk/commit/4f4e4263509fae890a241e3ea9dd7fc9d10d831e

## Issues Addressed:
- Should hopefully close
https://github.com/azerothcore/acore-docker/issues/12

## SOURCE:

## Tests Performed:
- I'm not exactly aware of the testing procedure for this, as I don't
use acore-docker
- That said, the previous behavior has been replicated:

https://github.com/azerothcore/azerothcore-wotlk/blob/163ecc7c55549212739688b21941e37c9ecbb3f8/apps/compiler/includes/functions.sh#L148-L150
- Open to suggestions


## How to Test the Changes:
1. Open to Suggestions

## Known Issues and TODO List:

- Test properly

## How to Test AzerothCore PRs
 
When a PR is ready to be tested, it will be marked as **[WAITING TO BE
TESTED]**.

You can help by testing PRs and writing your feedback here on the PR's
page on GitHub. Follow the instructions here:

http://www.azerothcore.org/wiki/How-to-test-a-PR

**REMEMBER**: when testing a PR that changes something **generic** (i.e.
a part of code that handles more than one specific thing), the tester
should not only check that the PR does its job (e.g. fixing spell XXX)
but **especially** check that the PR does not cause any regression (i.e.
introducing new bugs).

**For example**: if a PR fixes spell X by changing a part of code that
handles spells X, Y, and Z, we should not only test X, but **we should
test Y and Z as well**.

apps/compiler/includes/functions.sh
@@ -147,11 +147,25 @@ function comp_compile() {
 
       if [[ $DOCKER = 1 && $DISABLE_DOCKER_CONF != 1 ]]; then
         echo "Generating confs..."
+
+        # Search for all configs under DOCKER_ETC_FOLDER
         for dockerdist in "$DOCKER_ETC_FOLDER"/*.dockerdist; do
-          base_conf="$(echo "$dockerdist" | rev | cut -f1 -d. --complement | rev)"
-          cp -nv "$base_conf.dist" "$base_conf"
-          # Move configs from .conf.dockerdist to .conf
-          conf_layer "$dockerdist" "$base_conf" " # Copied from dockerdist"
+          # Grab "base" conf. turns foo.conf.dockerdist into foo.conf
+          baseConf="$(echo "$dockerdist" | rev | cut -f1 -d. --complement | rev)"
+          # env/dist/etc/foo.conf becomes foo.conf
+          filename="$(basename "$baseConf")"
+          # the dist files should be always found inside $confDir
+          # which may not be the same as DOCKER_ETC_FOLDER
+          distPath="$confDir/$filename.dist"
+          # if dist file doesn't exist, skip this iteration
+          [ ! -f "$distPath" ] && continue
+
+          # replace params in foo.conf.dist with params in foo.conf.dockerdist
+          conf_layer "$dockerdist" "$distPath" " # Copied from dockerdist"
+
+          # Copy modified dist file to $confDir/$filename
+          # Don't overwrite foo.conf if it already exists.
+          cp --no-clobber --verbose "$distPath" "$confDir/$filename"
         done
       fi
 
@@ -179,13 +193,18 @@ function conf_layer() {
   BASE="$2"
   COMMENT="$3"
 
+  # Loop over all defined params in conf file
   grep -E "^[a-zA-Z\.0-9]+\s*=.*$" "$LAYER" \
     | while read -r param
       do
+        # remove spaces from param
+        # foo       = bar becomes foo=bar
         NOSPACE="$(tr -d '[:space:]' <<< "$param")"
+
+        # split into key and value
         KEY="$(cut -f1 -d= <<< "$NOSPACE")"
         VAL="$(cut -f2 -d= <<< "$NOSPACE")"
-        # if key not in base or val not in line
+        # if key in base and val not in line
         if grep -qE "^$KEY" "$BASE" && ! grep -qE "^$KEY.*=.*$VAL" "$BASE"; then
           # Replace line
           # Prevent issues with shell quoting 